30e20aed8ecc0bcf75a61d1e1baab51a1bb6eba2
Jerry Lee <longpinkkirby@gmail.com>
2024-03-16 23:52:34 +0800

    Redesign Page, Add Hot Number Choose Options

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -37,7 +37,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_18692: string;
+	export const EFC_10592: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
@@ -109,6 +109,7 @@ declare module '$env/static/private' {
 	export const VSCODE_GIT_ASKPASS_NODE: string;
 	export const VSCODE_GIT_IPC_HANDLE: string;
 	export const windir: string;
+	export const __PSLockDownPolicy: string;
 }
 
 /**
@@ -151,7 +152,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_18692: string;
+		EFC_10592: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
@@ -223,6 +224,7 @@ declare module '$env/dynamic/private' {
 		VSCODE_GIT_ASKPASS_NODE: string;
 		VSCODE_GIT_IPC_HANDLE: string;
 		windir: string;
+		__PSLockDownPolicy: string;
 		[key: `PUBLIC_${string}`]: undefined;
 		[key: `${string}`]: string | undefined;
 	}
--- src/routes/mark-6-generator/+page.svelte
+++ src/routes/mark-6-generator/+page.svelte
@@ -1,32 +1,60 @@
 <script>
   // @ts-nocheck
-  // @ts-nocheck
+
+  let firstPress = true;
+  let ballAnimationTimeouts = [];
   const redNums = [
     1, 2, 7, 8, 12, 13, 18, 19, 23, 24, 29, 30, 34, 35, 40, 45, 46,
   ];
   const blueNums = [3, 4, 9, 10, 14, 15, 20, 31, 41, 42, 47, 48];
-  function func(num) {
+
+  /* 0: all
+     1: only hot
+     2: no hot*/
+  const ChooseOptions = {
+    All: 0,
+    OnlyHot: 1,
+    NoHot: 2,
+  };
+  let chooseOptions = ChooseOptions.NoHot;
+  function toggleHotNumbers(option) {
+    chooseOptions = option;
+  }
+
+  function getBallColor(num) {
     if (redNums.includes(num)) return "coral";
     if (blueNums.includes(num)) return "skyblue";
     return "limegreen";
   }
   const hotNumbers = [
-    49, 22, 10, 24, 30, 13, 9, 33, 6, 4, 14, 35, 12, 20, 28, 7, 34,
+    4, 6, 7, 9, 10, 12, 13, 14, 20, 22, 24, 28, 30, 33, 34, 35, 49,
   ];
   let currentNumbers = [];
 
-  async function resetBallsAnimation() {
+  function setBallsInvisible() {
+    if (firstPress) return;
+    for (let i = 0; i < 6; i++) {
+      const el = document.querySelector(`#ball-${i}`);
+      el.classList.add("hidden");
+    }
+  }
+  function resetBallsAnimation() {
+    for (const t of ballAnimationTimeouts) {
+      clearTimeout(t);
+    }
     for (let i = 0; i < 6; i++) {
       const el = document.querySelector(`#ball-${i}`);
       if (!el) continue;
-      el.classList.add("hidden");
       el.style.animation = "none";
-      setTimeout(() => {
-        el.classList.remove("hidden");
-        el.style.animation = "none";
-        el.offsetHeight; /* trigger reflow */
-        el.style.animation = null;
-      }, 200 * i);
+      ballAnimationTimeouts.push(
+        setTimeout(() => {
+          el.classList.remove("hidden");
+          el.style.animation = "none";
+          el.offsetHeight; /* trigger reflow */
+          el.style.animation = null;
+        }, 200 * i)
+      );
+      firstPress = false;
     }
   }
 
@@ -34,47 +62,66 @@
     const genNumbers = [];
     while (genNumbers.length < 6) {
       const num = Math.ceil(Math.random() * 49);
-      if (genNumbers.includes(num) || hotNumbers.includes(num)) continue;
+      if (genNumbers.includes(num)) continue;
+      if (chooseOptions === ChooseOptions.OnlyHot && !hotNumbers.includes(num))
+        continue;
+      if (chooseOptions === ChooseOptions.NoHot && hotNumbers.includes(num))
+        continue;
       genNumbers.push(num);
     }
     currentNumbers = genNumbers;
+    setBallsInvisible();
     setTimeout(() => {
       resetBallsAnimation();
     }, 0);
   }
 </script>
 
-<h2>六合彩生成器</h2>
-<div class="gen-btn-div">
-  <button class="gen-btn" on:click={generateNumbers}>發發發！</button>
-</div>
-<p>中獎號碼：</p>
-<div class="nums">
-  {#each currentNumbers as num, idx}
-    <p
-      id="ball-{idx}"
-      style="border: 2.5vw solid {func(num)};"
-      class="result-num ball-animation hidden"
-    >
-      {num}
-    </p>
-  {/each}
-</div>
+<body>
+  <h2>六合彩生成器</h2>
+  <div class="gen-btn-div">
+    <button class="gen-btn" on:click={generateNumbers}>發發發！</button>
+  </div>
+  <p>中獎號碼：</p>
+  <div class="nums">
+    {#each currentNumbers as num, idx}
+      <p
+        id="ball-{idx}"
+        style="border: 2.5vw solid {getBallColor(num)};"
+        class="result-num ball-animation hidden"
+      >
+        {num}
+      </p>
+    {/each}
+  </div>
+  <select name="" id="" bind:value={chooseOptions}>
+    <option value={ChooseOptions.All}>抽晒全部號碼</option>
+    <option value={ChooseOptions.NoHot}>唔抽熱門號碼</option>
+    <option value={ChooseOptions.OnlyHot}>淨係抽熱門號碼</option>
+  </select>
+  <p>熱門號碼: {hotNumbers}</p>
+</body>
 
 <style>
+  body {
+    height: 88vh;
+    display: flex;
+    flex-direction: column;
+  }
   .hidden {
     opacity: 0%;
   }
   .gen-btn-div {
+    flex: 1;
     margin: 0%;
     width: 100%;
     display: flex;
     justify-content: center;
   }
   .gen-btn {
-    width: 50vw;
-    height: 20vw;
-    font-size: 10vw;
+    width: 40%;
+    height: 100%;
+    font-size: 1rem;
   }
 
   h2 {
